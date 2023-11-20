b0fbd851c61307705920c27e929210a2719a9e42
nanano <alejandro.fonnegra@gmail.com>
2023-11-20 12:11:35 -0500

    bot 0.3 alpha

--- server/api/bot.post.ts
+++ server/api/bot.post.ts
@@ -195,4 +195,7 @@ async function sendWhatsAppMessage(recipient:number, message:any) {
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     console.log(body);
+    return {
+        "hello":"hello"
+    }
 });
