d1a66c48a2cb3cd11c929580c70ef84218637f7b
nanano <alejandro.fonnegra@gmail.com>
2023-11-20 18:13:52 -0500

    v0.47 alpha

--- server/api/bot.post.ts
+++ server/api/bot.post.ts
@@ -1,6 +1,6 @@
 import * as OpenAI from 'openai';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, query, where, getDocs, limit , doc, and } from 'firebase/firestore/lite';
+import { getFirestore, collection, query, where, getDocs, limit, doc, and } from 'firebase/firestore/lite';
 const config = useRuntimeConfig();
 
 const openai = new OpenAI.OpenAI({ apiKey: config.OPENAI });
@@ -15,8 +15,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function runConversation(phone:string, message:string) {
-    const messages:Array<any> = [
+async function runConversation(phone: string, message: string) {
+    const messages: Array<any> = [
         { role: 'system', content: 'Tu eres un asistente que listará y describirá sobre noticias de colombia y el mundo' },
         { role: 'system', content: 'No debes hablar sobre otros temas que no sean noticias. En caso de que el usuario te escriba algo fuera de alguna noticia, debes responder "Bienvenido a Colombia Al Día, por favor has tu consulta sobre que quieres saber en Colombia, el mundo, en deportes o el entretenimiento!. Puedes hacer solicitudes como "que ha pasado con el presidente hoy?" o "cuales son las noticias de política"."' },
         { role: 'system', content: 'Si te preguntan sobre una noticia especificamente debes describirla en detalle' },
@@ -26,7 +26,7 @@ async function runConversation(phone:string, message:string) {
         { role: "user", content: message },
     ];
 
-    const tools:Array<any> = [
+    const tools: Array<any> = [
         {
             type: "function",
             function: {
@@ -37,7 +37,7 @@ async function runConversation(phone:string, message:string) {
                     properties: {
                         data: {
                             type: "string",
-                            enum: ['politica','deportes','mundo','entretenimiento','todas'],
+                            enum: ['politica', 'deportes', 'mundo', 'entretenimiento', 'todas'],
                             description: "las categorías sobre las cuales el usuario puede buscar noticias",
                         },
                         date: {
@@ -72,7 +72,7 @@ async function runConversation(phone:string, message:string) {
         },
     ];
 
-    const response:any = await openai.chat.completions.create({
+    const response: any = await openai.chat.completions.create({
         model: "gpt-4",
         messages: messages,
         tools: tools,
@@ -85,7 +85,7 @@ async function runConversation(phone:string, message:string) {
     const toolCalls = responseMessage.tool_calls;
     if (responseMessage.tool_calls) {
 
-        const availableFunctions:any = {
+        const availableFunctions: any = {
             get_news: getNews,
             get_specific_news: getSpecificNews
         };
@@ -93,7 +93,7 @@ async function runConversation(phone:string, message:string) {
         messages.push(responseMessage); // extend conversation with assistant's reply
         for (const toolCall of toolCalls) {
             const functionName = toolCall.function.name;
-            const functionToCall:any = availableFunctions[functionName];
+            const functionToCall: any = availableFunctions[functionName];
             const functionArgs = JSON.parse(toolCall.function.arguments);
             const functionResponse = await functionToCall(
                 functionArgs.data,
@@ -112,55 +112,56 @@ async function runConversation(phone:string, message:string) {
             messages: messages,
         }); // get a new response from the model where it can see the function response
         const response = secondResponse.choices;
-        sendWhatsAppMessage(parseInt(phone),response[0].message.content);
-    } 
-    sendWhatsAppMessage(parseInt(phone),responseMessage);
+        sendWhatsAppMessage(parseInt(phone), response[0].message.content);
+    } else {
+        sendWhatsAppMessage(parseInt(phone), responseMessage.content);
+    }
 
 
 }
 
-async function getNews(category:string, date:string)  {
+async function getNews(category: string, date: string) {
     console.log(category, date);
-    let setQuery; 
-    if(category == 'todas') {
-        setQuery = query(collection(db, `news/`), where("date", "==", date), limit(5) );
+    let setQuery;
+    if (category == 'todas') {
+        setQuery = query(collection(db, `news/`), where("date", "==", date), limit(5));
     } else {
-        setQuery = query(collection(db, `news/`), and( where("category", "==", category), where("date", "==", date)), limit(5) );
+        setQuery = query(collection(db, `news/`), and(where("category", "==", category), where("date", "==", date)), limit(5));
     }
 
     const getNews = await getDocs(setQuery);
-    const result:Array<any> = [];
+    const result: Array<any> = [];
     getNews.forEach((doc) => {
-        result.push({ id: doc.id, news: doc.data()})     
+        result.push({ id: doc.id, news: doc.data() })
     });
     console.log(result)
     return JSON.stringify(result);
 }
 
-async function getSpecificNews(data:string, date:string)  {
-    let list; 
-    if(data.indexOf(',')>-1) {
+async function getSpecificNews(data: string, date: string) {
+    let list;
+    if (data.indexOf(',') > -1) {
         list = data.split(",");
     } else {
         list = [data]
     }
     console.log(list)
-    let setQuery = query(collection(db, `news/`), and( where("keywords", "array-contains-any", list), where("date", "==", date)), limit(5) );
+    let setQuery = query(collection(db, `news/`), and(where("keywords", "array-contains-any", list), where("date", "==", date)), limit(5));
     const getNews = await getDocs(setQuery);
-    const result:Array<any> = [];
+    const result: Array<any> = [];
     getNews.forEach((doc) => {
-        result.push({ id: doc.id, news: doc.data()})     
+        result.push({ id: doc.id, news: doc.data() })
     });
     return JSON.stringify(result);
 }
 
 function getDate() {
     const d = new Date();
-    return `${d.getMonth()+1}-${d.getDate()}-${d.getFullYear()} en formato MM-DD-AAAA`
+    return `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()} en formato MM-DD-AAAA`
 }
 
 
-async function sendWhatsAppMessage(recipient:number, message:any) {
+async function sendWhatsAppMessage(recipient: number, message: any) {
     const url = `https://graph.facebook.com/v17.0/111947191855209/messages`;
     const body = {
         messaging_product: "whatsapp",
@@ -185,7 +186,7 @@ async function sendWhatsAppMessage(recipient:number, message:any) {
             throw new Error(`Error: ${response.status}`);
         }
 
-        
+
         const data = await response.json();
         console.log('Message sent successfully:', data);
     } catch (error) {
@@ -195,23 +196,23 @@ async function sendWhatsAppMessage(recipient:number, message:any) {
 
 
 export default defineEventHandler(async (event) => {
-    const body:any = await readBody(event);
+    const body: any = await readBody(event);
     console.log(body);
     try {
-        if(body.entry.length>0) {
+        if (body.entry.length > 0) {
             const phone = body.entry[0].changes[0].value.messages.from;
             const msgType = body.entry[0].changes[0].value.messages[0].type;
             let message;
-            if(msgType == 'text') {
+            if (msgType == 'text') {
                 message = body.entry[0].changes[0].value.messages[0].text.body;
                 console.log(message);
                 await runConversation(phone, message)
             } else {
-                await sendWhatsAppMessage(parseInt(phone),'Lo Lamento solo recibo texto');
-            }   
+                await sendWhatsAppMessage(parseInt(phone), 'Lo Lamento solo recibo texto');
+            }
         }
 
-    } catch (e:any) {
+    } catch (e: any) {
         console.log(e);
     };
     return
